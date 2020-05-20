241800e32d6a0edcbe8024d58450d90dbcbdf741
Lily Ng <lilyng15@outlook.com>
2020-05-20 18:11:24 -0500

    updated files

---
+++ binomial_package_file/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author='Lily Ng',
+      author_email='lilyng15@outlook.com',
+      zip_safe=False)
